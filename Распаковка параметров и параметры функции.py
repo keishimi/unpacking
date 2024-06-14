0d408896979108f80c3a5a756dfe3a618dfcb3e5
Alex <alex.safonovaa@yandex.ru>
2024-06-14 18:45:52 +0300

    Add files via upload

--- Распаковка параметров и параметры функции.py
+++ Распаковка параметров и параметры функции.py
@@ -2,7 +2,7 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 values_list=[False, 52, 'run']
-values_dict={69, 'cloud', True}
+values_dict={'b':69, 'c':'cloud', 'a':True}
 
 values_list_2=[83, False]
 
@@ -10,5 +10,5 @@ print_params()
 print_params(b = 25)
 print_params(c = [1,2,3])
 print_params(*values_list)
-print_params(*values_dict)
+print_params(**values_dict)
 print_params(*values_list_2,42)
