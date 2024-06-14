a9a38a2090a8f34ac9b3777acf0a38dc946ab193
Alex <alex.safonovaa@yandex.ru>
2024-06-14 08:50:25 +0300

    Add files via upload

---
+++ Распаковка параметров и параметры функции.py
@@ -0,0 +1,14 @@
+
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+values_list=[False, 52, 'run']
+values_dict={69, 'cloud', True}
+
+values_list_2=[83, False]
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+print_params(*values_list)
+print_params(*values_dict)
+print_params(*values_list_2,42)
